33455275c8ffe92834876ca1ecf466f50d904b62
sixiwanzi-live <sixiwanzi.live@outlook.com>
2022-07-28 08:46:57 +0800

    增加明前奶绿的录播监控

--- archives.js
+++ archives.js
@@ -1,26 +1,30 @@
 export default [
     {
         id: 1,
-        url: 'https://api.bilibili.com/x/series/archives?mid=1660392980&series_id=1961996&sort=desc'
+        url: 'https://api.bilibili.com/x/series/archives?mid=1660392980&series_id=1961996&sort=desc&pn=1&ps=1'
     },
     {
         id: 2,
-        url: 'https://api.bilibili.com/x/series/archives?mid=1900141897&series_id=2001839&sort=desc'
+        url: 'https://api.bilibili.com/x/series/archives?mid=1900141897&series_id=2001839&sort=desc&pn=1&ps=1'
     },
     {
         id: 3,
-        url: 'https://api.bilibili.com/x/series/archives?mid=1878154667&series_id=2004017&sort=desc'
+        url: 'https://api.bilibili.com/x/series/archives?mid=1878154667&series_id=2004017&sort=desc&pn=1&ps=1'
     },
     {
         id: 4,
-        url: 'https://api.bilibili.com/x/series/archives?mid=1217754423&series_id=1975929&sort=desc'
+        url: 'https://api.bilibili.com/x/series/archives?mid=1217754423&series_id=1975929&sort=desc&pn=1&ps=1'
     },
     {
         id: 7,
-        url: 'https://api.bilibili.com/x/series/archives?mid=198297&series_id=210350&sort=desc'
+        url: 'https://api.bilibili.com/x/series/archives?mid=198297&series_id=210350&sort=desc&pn=1&ps=1'
+    },
+    {
+        id: 14,
+        url: 'https://api.bilibili.com/x/series/archives?mid=2132180406&series_id=2484684&sort=desc&pn=1&ps=1'
     },
     {
         id: 15,
-        url: 'https://api.bilibili.com/x/series/archives?mid=1219196749&series_id=2065795&sort=desc'
+        url: 'https://api.bilibili.com/x/series/archives?mid=1219196749&series_id=2065795&sort=desc&pn=1&ps=1'
     }
 ];
