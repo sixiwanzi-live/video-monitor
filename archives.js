791b013e533c97c996d64559c8e05067ce4de887
sixiwanzi-live <sixiwanzi.live@outlook.com>
2022-08-17 00:39:40 +0800

    增加麻尤米

--- archives.js
+++ archives.js
@@ -1,27 +1,33 @@
 export default [
     {
         id: 1,
-        url: 'https://api.bilibili.com/x/series/archives?mid=1660392980&series_id=1961996&sort=desc&pn=1&ps=1'
+        url: 'https://api.bilibili.com/x/series/archives?mid=1660392980&series_id=1961996&sort=desc&pn=1&ps=1',
+        mode: 1
     },
     {
         id: 2,
-        url: 'https://api.bilibili.com/x/series/archives?mid=1900141897&series_id=2001839&sort=desc&pn=1&ps=1'
+        url: 'https://api.bilibili.com/x/series/archives?mid=1900141897&series_id=2001839&sort=desc&pn=1&ps=1',
+        mode: 1
     },
     {
         id: 3,
-        url: 'https://api.bilibili.com/x/series/archives?mid=1878154667&series_id=2004017&sort=desc&pn=1&ps=1'
+        url: 'https://api.bilibili.com/x/series/archives?mid=1878154667&series_id=2004017&sort=desc&pn=1&ps=1',
+        mode: 1
     },
     {
         id: 4,
-        url: 'https://api.bilibili.com/x/series/archives?mid=1217754423&series_id=1975929&sort=desc&pn=1&ps=1'
+        url: 'https://api.bilibili.com/x/series/archives?mid=1217754423&series_id=1975929&sort=desc&pn=1&ps=1',
+        mode: 1
     },
     {
         id: 5,
-        url: 'https://api.bilibili.com/x/series/archives?mid=1230039261&series_id=2223855&sort=desc&pn=1&ps=1'
+        url: 'https://api.bilibili.com/x/series/archives?mid=1230039261&series_id=2223855&sort=desc&pn=1&ps=1',
+        mode: 1
     },
     {
         id: 7,
-        url: 'https://api.bilibili.com/x/series/archives?mid=198297&series_id=210350&sort=desc&pn=1&ps=1'
+        url: 'https://api.bilibili.com/x/series/archives?mid=198297&series_id=210350&sort=desc&pn=1&ps=1',
+        mode: 1
     },
     // {
     //     id: 9,
@@ -29,38 +35,52 @@ export default [
     // },
     {
         id: 10,
-        url: 'https://api.bilibili.com/x/series/archives?mid=1669777785&series_id=2519501&sort=desc&pn=1&ps=1'
+        url: 'https://api.bilibili.com/x/series/archives?mid=1669777785&series_id=2519501&sort=desc&pn=1&ps=1',
+        mode: 1
     },
     {
         id: 11,
-        url: 'https://api.bilibili.com/x/series/archives?mid=1778026586&series_id=2513879&sort=desc&pn=1&ps=1'
+        url: 'https://api.bilibili.com/x/series/archives?mid=1778026586&series_id=2513879&sort=desc&pn=1&ps=1',
+        mode: 1
     },
     {
         id: 12,
-        url: 'https://api.bilibili.com/x/series/archives?mid=1795147802&series_id=2516609&sort=desc&pn=1&ps=1'
+        url: 'https://api.bilibili.com/x/series/archives?mid=1795147802&series_id=2516609&sort=desc&pn=1&ps=1',
+        mode: 1
     },
     {
         id: 13,
-        url: 'https://api.bilibili.com/x/series/archives?mid=1811071010&series_id=2522257&sort=desc&pn=1&ps=1'
+        url: 'https://api.bilibili.com/x/series/archives?mid=1811071010&series_id=2522257&sort=desc&pn=1&ps=1',
+        mode: 1
     },
     {
         id: 14,
-        url: 'https://api.bilibili.com/x/series/archives?mid=2132180406&series_id=2484684&sort=desc&pn=1&ps=1'
+        url: 'https://api.bilibili.com/x/series/archives?mid=2132180406&series_id=2484684&sort=desc&pn=1&ps=1',
+        mode: 1
     },
     {
         id: 15,
-        url: 'https://api.bilibili.com/x/series/archives?mid=1219196749&series_id=2065795&sort=desc&pn=1&ps=1'
+        url: 'https://api.bilibili.com/x/series/archives?mid=1219196749&series_id=2065795&sort=desc&pn=1&ps=1',
+        mode: 1
     },
     {
         id: 16,
-        url: 'https://api.bilibili.com/x/series/archives?mid=1383815813&series_id=2060096&sort=desc&pn=1&ps=1'
+        url: 'https://api.bilibili.com/x/series/archives?mid=1383815813&series_id=2060096&sort=desc&pn=1&ps=1',
+        mode: 1
     },
     {
         id: 17,
-        url: 'https://api.bilibili.com/x/series/archives?mid=1011797664&series_id=2302706&sort=desc&pn=1&ps=1'
+        url: 'https://api.bilibili.com/x/series/archives?mid=1011797664&series_id=2302706&sort=desc&pn=1&ps=1',
+        mode: 1
     },
     {
         id: 19,
-        url: 'https://api.bilibili.com/x/series/archives?mid=1535525542&series_id=2205073&sort=desc&pn=1&ps=1'
+        url: 'https://api.bilibili.com/x/series/archives?mid=1535525542&series_id=2205073&sort=desc&pn=1&ps=1',
+        mode: 1
+    },
+    {
+        id: 20,
+        url: 'https://api.bilibili.com/x/polymer/space/seasons_archives_list?mid=7262655&season_id=501913&sort_reverse=false&page_num=1&page_size=1',
+        mode: 2
     }
 ];
