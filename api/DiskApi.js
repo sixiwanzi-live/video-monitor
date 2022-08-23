cbb9230724cd073a344f7e3fcafdf7e898af329d
sixiwanzi-live <sixiwanzi.live@outlook.com>
2022-08-24 00:56:38 +0800

    dev

--- api/DiskApi.js
+++ api/DiskApi.js
@@ -2,9 +2,16 @@ import axios from 'axios';
 import config from '../config.js';
 
 export default class DiskApi {
-    static async save(bv) {
+
+    static async saveByBv(bv) {
         const url = `${config.disk.url}/disks`;
-        const res = await axios.post(url, {bv}, {timeout:600 * 1000});
+        const res = await axios.post(url, {bv}, {timeout:60 *60 * 1000});
+        return res.data;
+    }
+
+    static async saveByUrl(downloadUrl) {
+        const url = `${config.disk.url}/disks`;
+        const res = await axios.post(url, {url: downloadUrl}, {timeout:60 * 60 * 1000});
         return res.data;
     }
 }
