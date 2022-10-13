fbc84db77ae73f701f5e01eaa4264fdf5b535faa
sixiwanzi-live <sixiwanzi.live@outlook.com>
2022-10-13 09:01:23 +0800

    录播站增加了按月份分类，监控程序需要增大per_page数量

--- replay.js
+++ replay.js
@@ -78,22 +78,23 @@ const archives = [
                 page: 1,
                 password: '',
                 path: `/${archive.path}`,
-                per_page: 10,
+                per_page: 90,
                 refresh: false
             };
             try {
+                console.log(params);
                 const res = await axios.post(url, params);
                 const file = res.data.data.content.filter(file => !file.is_dir && file.type === 2 && file.size > 0).at(0);
                 console.log(file);     
                 const downloadUrl = `https://bili.lubo.media/d/${archive.path}/${file.name}`;
                 // 下载视频
-                try {
-                    await DiskApi.saveByUrl(downloadUrl);
-                } catch (ex) {
-                    console.log(ex);
-                    PushApi.push(`下载"${title}"视频失败`, ex.response.data);
-                    continue;
-                }
+                // try {
+                //     await DiskApi.saveByUrl(downloadUrl);
+                // } catch (ex) {
+                //     console.log(ex);
+                //     PushApi.push(`下载"${title}"视频失败`, ex.response.data);
+                //     continue;
+                // }
             } catch (ex) {
                 console.log(ex);
                 PushApi.push(`请求回放列表失败`, ex.response.data);
