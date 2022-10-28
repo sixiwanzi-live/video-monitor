cabea32e93b28dbef2a1ea0a5cacb90d2bbe587c
sixiwanzi-live <sixiwanzi.live@outlook.com>
2022-10-28 18:17:48 +0800

    m3u8

--- m3u8.js
+++ m3u8.js
@@ -1,7 +1,7 @@
 import { readdir } from 'fs/promises';
 
 (async () => {
-    const path = '/mnt/data0/5/露早GOGO/';
+    const path = '/mnt/data0/5/露早GOGO/2022-10';
     const files = await readdir(path);
     console.log(files);
 })();
