a19ce0b4736bb68caef1474308f8eec1d3337542
sixiwanzi-live <sixiwanzi.live@outlook.com>
2022-10-28 18:37:58 +0800

    dev

--- m3u8.js
+++ m3u8.js
@@ -1,7 +1,20 @@
-import { readdir } from 'fs/promises';
+import { readdir, mkdir } from 'fs/promises';
+import {spawn} from 'child_process';
 
 (async () => {
     const path = '/mnt/data0/5/露早GOGO/2022-10';
     const files = await readdir(path);
-    console.log(files);
+    for (let i = 0; i < files.length; ++i) {
+        const file = files[i];
+        if (file.indexOf('.mp4') === -1) {
+            continue;
+        }
+        const mp4 = `${path}/${file}`;
+        // 创建文件夹
+        const dir = mp4.replace('.mp4', '');
+        await mkdir(dir);
+        console.log(`create dir ${dir}`);
+        const m3u8 = `${dir}/index.m3u8`;
+        console.log(`create m3u8 ${m3u8}`);
+    }
 })();
