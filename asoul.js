5fe0e7fd065120d6d9773dc9fe6d8dba5200e902
sixiwanzi-live <sixiwanzi.live@outlook.com>
2023-03-07 09:38:46 +0800

    dev

--- asoul.js
+++ asoul.js
@@ -3,26 +3,29 @@ import moment from "moment";
 import ZimuApi from "./api/ZimuApi.js";
 
 (async () => {
+    const authorId = 34;
     try {
-        const res = await axios.get("https://api.matsuri.icu/channel/1960682407/clips");
+        const res = await axios.get("https://api.matsuri.icu/channel/1501380958/clips");
         const items = res.data.data;
         for (let i = 0; i < items.length; ++i) {
             const item = items[i];
             const title = item.title;
             const datetime = moment(item.start_time).format("YYYY-MM-DD HH:mm:ss");
             const cover = item.cover.substring(8);
-            const clip = {
-                authorId: 34,
-                title: title,
-                cover: cover,
-                datetime: datetime
-            };
-            console.log(clip);
-            try {
-                await ZimuApi.insertClip(clip);
-                console.log("写入成功");
-            } catch (ex) {
-                console.log(ex.response.data);
+            if (datetime > '2022') {
+                const clip = {
+                    authorId: authorId,
+                    title: title,
+                    cover: cover,
+                    datetime: datetime
+                };
+                console.log(clip);
+                try {
+                    await ZimuApi.insertClip(clip);
+                    console.log("写入成功");
+                } catch (ex) {
+                    console.log(ex.response.data);
+                }
             }
         }
     } catch (ex) {
